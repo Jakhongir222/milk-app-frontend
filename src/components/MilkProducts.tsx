bd596f90d6f80df1791505fba9d8875757ebd1e5
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2023-01-25 15:16:01 +0100

    Filter is also done

--- src/components/MilkProducts.tsx
+++ src/components/MilkProducts.tsx
@@ -6,42 +6,63 @@ import milkImage from './milk.png'
 export default function MilkProducts () {
     const [products, setProducts] = useState<MilkInterface[]>([]);
     const [search, setSearch] = useState('');
-    const [isLoading, setLoading] = useState(false)
+    const [filter, setFilter] = useState('');
+    const [isLoading, setLoading] = useState(false);
+    const [types, setTypes] = useState<string[]>([]);
+    const [showTypes, setShowTypes] = useState(false);
     const baseUrl = 'http://localhost:8080/milk';
 
     useEffect(()=>{
         setLoading(true);
         fetch(baseUrl)
             .then(data=> data.json())
-            .then((data) => 
-            setProducts(data))
-                setLoading(false)
+            .then((data: MilkInterface[]) => {
+                setProducts(data);
+                setTypes(Array.from(new Set(data.map(product => product.type))));
+            })
+        setLoading(false)
     }, [])
+    
 
     if(isLoading) return <p>Loading</p>
     if(!products) return <p>There are no Milk</p> 
 
-    const filteredProducts = products.filter(product => {
-        return product.name.toLowerCase().includes(search.toLowerCase()) || 
-               product.type.toLowerCase().includes(search.toLowerCase())
-    });
+    const filteredProducts = products
+    .filter(product => product.name.toLowerCase().includes(search.toLowerCase()))
+    .filter(product => filter.toLowerCase() === "" || product.type.toLowerCase().includes(filter.toLowerCase()));
 
-  return (
-    <div className='milk-container'>
-        <input 
-            type="text" 
-            placeholder="Search by name or type" 
-            value={search} 
-            onChange={e => setSearch(e.target.value)}
-            className="search-input"
-        />
-        {filteredProducts.map((product) => (
-            <div key={product.id} className='milk-card' >
-                <img className= "milk-image" src={milkImage}/>
-                <div className= "milk-name">{product.name}</div>
-                <div className= "milk-type">{product.type}</div>
-                <div className= "milk-storage">{product.storage + " liters"}</div>
-            </div>))}
-    </div>
-  )
+    return (
+        <div className='milk-container'>
+            <input 
+                type="text" 
+                placeholder="Search by name" 
+                value={search} 
+                onChange={e => setSearch(e.target.value)}
+                className="search-input"
+            />
+            <button className="filter-button" onClick={() => setShowTypes(!showTypes)}>Filter by type</button>
+            {showTypes && (
+                <div className="types-container">
+                    {types.map(type => (
+                        <div 
+                            key={type} 
+                            className="type"
+                            onClick={() => setFilter(type)}
+                        >
+                            {type}
+                        </div>
+                    ))}
+                </div>
+            )}
+            {filteredProducts.map((product) => (
+                <div key={product.id} className='milk-card' >
+                    <img className= "milk-image" src={milkImage}/>
+                    <div className= "milk-name">{product.name}</div>
+                    <div className= "milk-type">{product.type}</div>
+                    <div className= "milk-storage">{product.storage + " liters"}</div>
+                </div>))}
+        </div>
+      )
+    
+    
 }
